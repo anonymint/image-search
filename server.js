42bb01c2fde3c2bf6aa5f2a3590ca4cfaf6c3aab
Anonymint <mint.com@gmail.com>
2016-01-30 14:37:23 +0700

    Change .env file

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use('/public', express.static(process.cwd() + 'public'));
 
 routes(app);
 
+console.log(process.env.VARMINT);
+console.log(process.env.KEY_500PX);
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log('App server is running on port ' + port + '...');
